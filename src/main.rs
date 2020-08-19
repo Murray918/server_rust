4d711b30f312ac630d453be2768ae954fe5ef406
Andrew Murray <darbyguitars@gmail.com>
2020-08-19 15:06:01 -0500

    implement basic run function and server struct with associated funtions and methods
     Changes to be committed:
    	new file:   Cargo.lock
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let address = String::from("120.0.0.1:8080".to_string());
+
+    let server = Server::new(address);
+
+    server.run()
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self { addr: addr }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr)
+    }
 }
