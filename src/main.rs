5994b7e213af0c7e0c97320453efa9d6552c7392
Andrew Murray <darbyguitars@gmail.com>
2020-08-20 14:09:24 -0500

    implement error handling for request parsing
     Changes to be committed:
    	modified:   src/http/mod.rs
    	modified:   src/http/request.rs
    	modified:   src/main.rs
    	modified:   src/server.rs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod http;
 mod server;
 
 fn main() {
-    let server = Server::new("120.0.0.1:8080".to_string());
+    let server = Server::new("127.0.0.1:8080".to_string());
     server.run()
 }
 
--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,47 @@
-    pub struct Server {
-        addr: String,
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::io::Read;
+use std::net::TcpListener;
+
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
     }
 
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Self { addr }
-        }
+    pub fn run(self) {
+        println!("Listening on {}", self.addr);
 
-        pub fn run(self) {
-            println!("Listening on {}", self.addr)
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Recieved request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(error) => println!("Failed to parse request: {}", error),
+                            }
+                        }
+                        Err(error) => println!("Failed to read from connection: {}", error),
+                    };
+                }
+                Err(error) => println!("ERROR=> Failed to establish connection{}", error),
+            }
+            let res = listener.accept();
+
+            if res.is_err() {
+                continue;
+            }
+
+            let (stream, addr) = res.unwrap();
         }
     }
+}
